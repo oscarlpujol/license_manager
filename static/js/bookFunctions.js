16d164ac55a5d21f74a729b46c581dba69623afc
oscarlpujol <oscarlahuerta@gmail.com>
2024-01-27 17:50:36 +0100

    Changed index.html to a form and adding tasks

---
+++ static/js/bookFunctions.js
@@ -0,0 +1,8 @@
+// function addBook(){
+//     AQUI HABRÍA QUE COGER EL NÚMERO DEL ÚLTIMO VALOR DE LA TABLA
+//     $('table > tbody').append(`
+//         <th><input type="text" name="title_X" required></th>
+//         <th><input type="text" name="isbn_X" required></th>
+//         <th><input type="text" name="num_lic_X" required></th>
+//     `)
+// }
